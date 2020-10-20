81bfa166dea4e0bad9311a42553e6e1135f7829b
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-10-21 00:57:12 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan15-Logika/src/if1/pkg10117126/latihan15/logika/IF110117126Latihan15Logika.java
@@ -0,0 +1,26 @@
+/*
+ * 
+ * @author
+ * NAMA : Mawa Faqu Rochman
+ * KELAS : PBO1
+ * NIM : 10117126
+ *
+ */
+package if1.pkg10117126.latihan15.logika;
+
+public class IF110117126Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
